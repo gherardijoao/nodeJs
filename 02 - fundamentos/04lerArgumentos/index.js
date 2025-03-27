62c9a5f916241be1d3ff6e27ee1676e8768c2c35
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-03-26 22:51:40 -0300

    ler argumentos

---
+++ 02 - fundamentos/04lerArgumentos/index.js
@@ -0,0 +1,11 @@
+console.log(process.argv);
+
+const args = process.argv.slice(2);
+
+console.log(args);
+
+const nome = args[0].split("=")[1];
+
+console.log(nome);
+
+console.log(`o nome dele eh ${nome}`);
