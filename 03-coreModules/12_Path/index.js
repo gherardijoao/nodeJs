a54ea71453f418f7996c38148768ff9445c56eae
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-04-18 22:30:14 -0300

    modulo path

---
+++ 03-coreModules/12_Path/index.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+const customPath = "/relatorios/joao/relatorio1.pdg";
+
+console.log(path.dirname(customPath));
+console.log(path.basename(customPath));
+console.log(path.extname(customPath));
